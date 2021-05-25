52930b844e350f82c00e65a5856f7ac82b508c97
AMAL REJI <47815969+amalreji017@users.noreply.github.com>
2021-05-26 01:32:45 +0530

    token expiry

--- backend/services/token.service.js
+++ backend/services/token.service.js
@@ -1,7 +1,7 @@
 const tokenize = require('jsonwebtoken');
 const dotenv = require('dotenv')
 dotenv.config();
-let key = process.env.key;
+let key = process.env.KEY;
 
 exports.encode = (data) => {
     return tokenize.sign({student_name:data.student_name,college_id:data.college_id},secret,{ expiresIn: '1h' })
