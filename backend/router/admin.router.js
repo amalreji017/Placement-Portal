1c54c31b7953e5a338e3ff2184206f0217c030f1
nadirsha <nadirshaaliyar747@gmail.com>
2021-05-31 13:53:55 +0530

    admin:add faculty

--- backend/router/admin.router.js
+++ backend/router/admin.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const adminHandler = require('../handlers/admin/admin.handler');
+const facultyHandler=require('../handlers/faculty.handler')
 const { protect } = require("../middleware/auth");
 const router =  express.Router();
 
@@ -11,6 +12,7 @@ const router =  express.Router();
 //router.post('/insert',adminHandler.insert);*/
 router.post('/login',adminHandler.login);
 router.get('/dashboard',protect,adminHandler.getPrivateRoute);
+router.post('/insert',protect,facultyHandler.insert);
 
 
 
